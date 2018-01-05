16575c024e522579f125f30b5879b8040f4054e1
IngCr3at1on <ingcr3at1on@gmail.com>
2018-01-04 19:48:35 -0600

    first pass at setting up travis

---
+++ scripts/travis_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+cd $GOPATH/src/github.com/IngCr3at1on/glas-cli
+go build
---
+++ scripts/travis_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+go get -u github.com/golang/dep/cmd/dep
+cd $GOPATH/src/github.com/IngCr3at1on/glas-cli
+dep ensure
